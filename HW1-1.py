9682e9ff24f2e3a2d20b77c6652f81815b871031
RamirezElijah5248 <97366596+RamirezElijah5248@users.noreply.github.com>
2022-08-30 11:16:32 -0500

    Delete HW1-1.py

--- HW1-1.py
+++
@@ -1,6 +0,0 @@
-fname = input("Input your First Name : ")
-lname = input("Input your Last Name : ")
-print ("Hello  " + lname + " " + fname)
-print("what is your favorite number?")
-fav_num = input("input favorite number : ")
-print("oh, Mr. " + lname + " your favorite number is " + fav_num)
